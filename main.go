110f273fb5cc7124bbf97de0d9791210828e52d9
jyami <eiji@jyami.net>
2024-02-26 16:08:34 +0900

    飛んでくるシグナルを調べる

--- main.go
+++ main.go
@@ -5,10 +5,56 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"syscall"
 )
 
+var signals = []os.Signal{
+	syscall.SIGABRT,
+	syscall.SIGALRM,
+	syscall.SIGBUS,
+	//syscall.SIGCHLD, 通常は無視
+	//syscall.SIGCLD, 通常は無視
+	syscall.SIGCONT,
+	syscall.SIGFPE,
+	syscall.SIGHUP,
+	syscall.SIGILL,
+	//syscall.SIGIO, 通常は無視
+	syscall.SIGIOT,
+	syscall.SIGINT,
+	syscall.SIGKILL,
+	syscall.SIGPIPE,
+	syscall.SIGPOLL,
+	syscall.SIGPROF,
+	syscall.SIGPWR,
+	syscall.SIGQUIT,
+	syscall.SIGSEGV,
+	syscall.SIGSTKFLT,
+	syscall.SIGSTOP,
+	syscall.SIGSYS,
+	syscall.SIGTERM,
+	syscall.SIGTSTP,
+	syscall.SIGTTIN,
+	syscall.SIGTTOU,
+	syscall.SIGUNUSED,
+	//syscall.SIGURG, 通常は無視
+	syscall.SIGTRAP,
+	syscall.SIGUSR1,
+	syscall.SIGUSR2,
+	syscall.SIGVTALRM,
+	//syscall.SIGWINCH, 通常は無視
+	syscall.SIGXCPU,
+	syscall.SIGXFSZ,
+}
+
 func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, signals...)
+	go func() {
+		s := <-sigs
+		log.Fatalf("got signal:%s\n", s)
+		panic(-1)
+	}()
 
 	reader := bufio.NewReader(os.Stdin)
 
